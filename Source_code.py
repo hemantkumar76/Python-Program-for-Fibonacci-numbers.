dc0e46e1d3d18b5ec5d29cb285a65c7ef281e70f
Hemant <92645502+hemantkumar76@users.noreply.github.com>
2022-02-23 21:10:00 +0530

    Create Source_code.py

---
+++ Source_code.py
@@ -0,0 +1,18 @@
+# assignment 3 Hemant MY CAPTAIN
+FibArray = [0, 1]
+ 
+def fibonacci(n):
+   
+ 
+    if n <= 0:
+        print("Incorrect input")
+         
+   
+    elif n <= len(FibArray):
+        return FibArray[n - 1]
+    else:
+        temp_fib = fibonacci(n - 1) + fibonacci(n - 2)
+        FibArray.append(temp_fib)
+        return temp_fib
+ 
+print(fibonacci(9))
